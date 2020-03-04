2185dbe8e0d58aa55f616313b6e37fdb2e3626ce
Sascha Goldhofer <post@saschagoldhofer.de>
2020-03-04 13:34:09 +0100

    Update tests

--- test/handler/unsplash.test.ts
+++ test/handler/unsplash.test.ts
@@ -4,7 +4,7 @@ import unsplashHandler from "../../src/handler/unsplash";
 
 
 describe("Handler.unsplash", () => {
-    const UNSPLASH_URL = "https://images.unsplash.com/photo-1583253684016-54b500ca4842?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    const UNSPLASH_URL = "https://images.unsplash.com/photo-1583253684016-54b500ca4842?auto=format&fit=crop&w=800&q=60"
 
     it("should register on unplash photo url", () => {
         const use = unsplashHandler.use({ url: UNSPLASH_URL });
@@ -15,6 +15,28 @@ describe("Handler.unsplash", () => {
         const use = unsplashHandler.use({ url: "https://media.istockphoto.com/photos/apples-isolated-on-white-background-picture-id902183704" });
         assert.ok(use === false);
     });
+
+
+    describe("getImageURL", () => {
+
+        it("should return input url", async () => {
+            const url = await unsplashHandler.getImageURL({ url: UNSPLASH_URL });
+            assert.equal(url, UNSPLASH_URL);
+        });
+
+        it("should return add additional property", async () => {
+            const url = await unsplashHandler.getImageURL({ url: UNSPLASH_URL, height: 400 });
+            const params = new URLSearchParams(url);
+            assert.equal(params.get("h"), "400");
+        });
+
+        it("should return update existing property", async () => {
+            const url = await unsplashHandler.getImageURL({ url: UNSPLASH_URL, width: 360 });
+            const params = new URLSearchParams(url);
+            assert.equal(params.get("w"), "360");
+        });
+    });
+
 
     describe("getImageInfo", () => {
 
