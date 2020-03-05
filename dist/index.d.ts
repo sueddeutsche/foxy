692efbac6e430c24f73365230d007aae2996b01b
Sascha Goldhofer <post@saschagoldhofer.de>
2020-03-05 06:36:46 +0100

    Add expose request interfaces

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,9 @@
 import Foxy, { Options } from "./Foxy";
 import utils from "./utils";
-import Handler from "./handler/Handler";
+import Handler, { Request, URLRequest, InfoRequest } from "./handler/Handler";
 declare const handler: {
     unsplash: Handler;
     image: Handler;
     video: Handler;
 };
-export { Foxy, Options, Handler, handler, utils };
+export { Foxy, Options, Handler, Request, URLRequest, InfoRequest, handler, utils };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Foxy, { Options } from "./Foxy";
 import utils from "./utils";
-import Handler from "./handler/Handler";
+import Handler, { Request, URLRequest, InfoRequest } from "./handler/Handler";
 import unsplash from "./handler/unsplash";
 import image from "./handler/imageHandler";
 import video from "./handler/videoHandler";
@@ -15,6 +15,9 @@ export {
     Foxy,
     Options,
     Handler,
+    Request,
+    URLRequest,
+    InfoRequest,
     handler,
     utils
 };
